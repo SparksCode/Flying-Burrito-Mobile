d909eec10b80cbe4b98c832dfb7aeb08c55557ee
SparksCode <overeck@gmail.com>
2017-11-16 11:54:36 -0600

    Creation of FoodViewHolder.

---
+++ app/src/main/java/com/flyingburritoco/mobilemenu/ViewHolder/FoodViewHolder.java
@@ -0,0 +1,40 @@
+package com.flyingburritoco.mobilemenu.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.flyingburritoco.mobilemenu.Interface.ItemClickListener;
+import com.flyingburritoco.mobilemenu.R;
+
+/**
+ * Created by Zach on 11/16/2017.
+ */
+
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtFoodName;
+    public ImageView imageView;
+
+    private ItemClickListener itemClickListener;
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public FoodViewHolder(View itemView) {
+        super(itemView);
+
+        txtFoodName = (TextView)itemView.findViewById(R.id.food_name);
+        imageView = (ImageView)itemView.findViewById(R.id.food_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+
+    }
+}
