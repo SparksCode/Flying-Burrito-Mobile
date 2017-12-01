5aac6cfb235910e99e9daa54ccb2cfd6c7b3e24a
SparksCode <overeck@gmail.com>
2017-12-01 16:55:18 -0600

    Implementation of Request. Cart Activity methods expanded.

--- app/src/main/java/com/flyingburritoco/mobilemenu/Cart.java
+++ app/src/main/java/com/flyingburritoco/mobilemenu/Cart.java
@@ -1,13 +1,21 @@
 package com.flyingburritoco.mobilemenu;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.flyingburritoco.mobilemenu.Common.Common;
 import com.flyingburritoco.mobilemenu.Database.Database;
 import com.flyingburritoco.mobilemenu.Model.Order;
+import com.flyingburritoco.mobilemenu.Model.Request;
 import com.flyingburritoco.mobilemenu.ViewHolder.CartAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,7 +34,7 @@ public class Cart extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
 
     FirebaseDatabase database;
-    DatabaseReference request;
+    DatabaseReference requests;
 
     TextView txtTotalPrice;
     FButton btnPlace;
@@ -41,7 +49,7 @@ public class Cart extends AppCompatActivity {
 
         //Firebase
         database = FirebaseDatabase.getInstance();
-        request = database.getReference("Request");
+        requests = database.getReference("Request");
 
         recyclerView = (RecyclerView)findViewById(R.id.listCart);
         recyclerView.setHasFixedSize(true);
@@ -51,7 +59,62 @@ public class Cart extends AppCompatActivity {
         txtTotalPrice = (TextView)findViewById(R.id.total);
         btnPlace = (FButton)findViewById(R.id.btnPlaceOrder);
 
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertDialog();
+            }
+        });
+
         loadListFood();
+    }
+
+    private void showAlertDialog(){
+            AlertDialog.Builder alertDialog =new AlertDialog.Builder(Cart.this);
+            //alertDialog.setTitle("");
+            alertDialog.setMessage("Enter your address: ");
+
+            final EditText edtAddress = new EditText(Cart.this);
+
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT
+            );
+
+            edtAddress.setLayoutParams(lp);
+            alertDialog.setView(edtAddress);
+            alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+            alertDialog.setPositiveButton("CANCEL", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+
+            alertDialog.setNegativeButton("ENTER", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    Request request = new Request(
+                            Common.currentUser.getPhone(),
+                            Common.currentUser.getName(),
+                            edtAddress.getText().toString(),
+                            txtTotalPrice.getText().toString(),
+                            cart
+                    );
+
+                    requests.child(String.valueOf(System.currentTimeMillis()))
+                    .setValue(request);
+
+                    new Database(getBaseContext()).cleanCart();
+                    Toast.makeText(Cart.this, "Your Order Has Been Placed", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+
+            alertDialog.show();
     }
 
     private void loadListFood(){
