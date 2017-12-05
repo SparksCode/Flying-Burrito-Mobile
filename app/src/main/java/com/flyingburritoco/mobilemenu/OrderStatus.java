8ac0db549b44102f8398a826cc8ca28b1be8bbcf
SparksCode <overeck@gmail.com>
2017-12-05 13:23:53 -0600

    Update Order Status to include return to home button.

--- app/src/main/java/com/flyingburritoco/mobilemenu/OrderStatus.java
+++ app/src/main/java/com/flyingburritoco/mobilemenu/OrderStatus.java
@@ -1,9 +1,12 @@
 package com.flyingburritoco.mobilemenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.flyingburritoco.mobilemenu.Common.Common;
@@ -21,6 +24,8 @@ public class OrderStatus extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference requests;
 
+    Button exitOrders;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +40,16 @@ public class OrderStatus extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         loadOrders(Common.currentUser.getPhone());
+
+
+        exitOrders = (Button)findViewById(R.id.exitOrders);
+        exitOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(OrderStatus.this, Home.class);
+                startActivity(home);
+            }
+        });
     }
 
     private void loadOrders(String phone) {
